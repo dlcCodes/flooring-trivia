ddb8f920bf18afa53ba6dbb5c02bc6c7be668ccd
dlcCodes <daniel.n.lundy@gmail.com>
2023-10-08 10:37:05 -0400

    Set up project. Started json data entry.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+console.log("connected");
+
+//* async data for page. calling data for the trivia
+
+async function loadQuestions() {
+    const response = await fetch('/../tests/practice-test.json');
+    const questionsAsk = await response.json();
+
+    console.log(questionsAsk);
+}
+
+loadQuestions();
